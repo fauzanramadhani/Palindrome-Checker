b97e977b41cf6d89e5e7fd3b48d3e5a8f90f4fb1
fauzangraciaramadhani <fauzanramadhani06@gmail.com>
2023-12-28 12:33:38 +0700

    feat: Implement UI/UX for third screen and implement logic for first screen

--- app/src/main/java/com/fgr/palindromechecker/feature/first/FirstScreen.kt
+++ app/src/main/java/com/fgr/palindromechecker/feature/first/FirstScreen.kt
@@ -3,30 +3,45 @@ package com.fgr.palindromechecker.feature.first
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import com.fgr.palindromechecker.R
 import com.fgr.palindromechecker.databinding.ActivityFirstScreenBinding
 import com.fgr.palindromechecker.feature.second.SecondScreen
+import com.fgr.palindromechecker.utils.isPalindrome
 
 class FirstScreen : AppCompatActivity() {
     private lateinit var binding: ActivityFirstScreenBinding
-    private lateinit var intent: Intent
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFirstScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        intent = Intent()
         binding.firstScreenBtnCheck.setOnClickListener {
             hideKeyboard()
+            val text = binding.firstScreenEdPalindrome.text.toString()
+            if (text.isNotEmpty()) {
+                binding.firstScreenResultLayout.visibility = View.VISIBLE
+                if (text.isPalindrome()) {
+                    binding.firstScreenResultText.setText(R.string.true_palindrome)
+                } else {
+                    binding.firstScreenResultText.setText(R.string.false_palindrome)
+                }
+            } else {
+                binding.firstScreenEdPalindrome.error = getString(R.string.empty_palindrome)
+            }
+        }
+        binding.firstScreenResultCloseButton.setOnClickListener {
+            binding.firstScreenResultLayout.visibility = View.GONE
         }
         binding.firstScreenBtnNext.setOnClickListener {
             val username = binding.firstScreenEdName.text.toString()
             if (username.isNotEmpty()) {
-                intent = Intent(this, SecondScreen::class.java)
+                val intent = Intent(this, SecondScreen::class.java)
                 intent.putExtra(SecondScreen.EXTRAS.USERNAME, username)
                 startActivity(intent)
             } else {
-                binding.firstScreenEdName.error = "Username tidak boleh kosong"
+                binding.firstScreenEdName.error = getString(R.string.empty_username)
             }
         }
     }
--- app/src/main/java/com/fgr/palindromechecker/utils/utils.kt
+++ app/src/main/java/com/fgr/palindromechecker/utils/utils.kt
@@ -1,2 +1,7 @@
 package com.fgr.palindromechecker.utils
 
+fun String.isPalindrome(): Boolean {
+    val cleanedInput = this.replace("\\s".toRegex(), "").lowercase()
+
+    return cleanedInput == cleanedInput.reversed()
+}
